e942976eb69ec6cc52a2f60d8d8dd8be3c2fd922
andrr64 <derzandrr03@gmail.com>
2025-01-27 13:09:00 +0700

    update rute

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from database import Base, engine
+from route import user
+
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+Base.metadata.create_all(bind=engine)
+
+app.include_router(user.router, prefix="/user", tags=["user"])
